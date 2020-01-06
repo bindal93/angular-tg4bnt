37646a65d41194e46049d6172e8233430748ef21
bindal93 <shivam.bindal@yahoo.com>
2020-01-06 18:22:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-tg4bnt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-tg4bnt)
---
+++ src/app/datepicker-overview-example.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+
+/** @title Basic datepicker */
+@Component({
+  selector: 'datepicker-overview-example',
+  templateUrl: 'datepicker-overview-example.html',
+  styleUrls: ['datepicker-overview-example.css'],
+})
+export class DatepickerOverviewExample {
+
+   startDate1 = new Date(1990, 0, 1);
+}
+
+
+/**  Copyright 2019 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
